f0dab20b8c628b1feacbe7219beb852c86c2b7ac
Daniela Huber <156130469+Dani0825@users.noreply.github.com>
2026-02-04 21:41:50 +0000

    class 02/04

--- 07_Queue/QueueArray.hpp
+++ 07_Queue/QueueArray.hpp
@@ -5,11 +5,17 @@ template <typename T, int size = 100>
 class QueueArray {
 public:
 QueueArray();
-int size() const;
-bool empty() const;
-bool full() const;
-void enqueue(const T& val);
-const T& dequeue();
+
+int         length() const;  //TODO
+bool        empty() const;
+bool        full() const;
+void        print() const;
+void        clear(); //TODO
+
+void        enqueue(const T& val);
+const T&    dequeue();
+const T&    front(); // Return the first element in the queue TODO
+
 private:
 T data[size]; // An array of 100 elements
 int queue_size;
